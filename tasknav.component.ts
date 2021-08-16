5f687733a9e3cc61d46bc154d7668625a7afca4c
7058471639 <86941775+7058471639@users.noreply.github.com>
2021-08-16 16:23:31 +0530

    Add files via upload

---
+++ tasknav.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+
+@Component({
+  selector: 'app-tasknav',
+  templateUrl: './tasknav.component.html',
+  styleUrls: ['./tasknav.component.css']
+})
+export class TasknavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  opennav(){
+
+  }
+}
