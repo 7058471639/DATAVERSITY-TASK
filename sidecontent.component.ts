0d24581b5c29b15dba458b7eb20df2abe1848e0a
7058471639 <86941775+7058471639@users.noreply.github.com>
2021-08-16 16:22:34 +0530

    Add files via upload

---
+++ sidecontent.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sidecontent',
+  templateUrl: './sidecontent.component.html',
+  styleUrls: ['./sidecontent.component.css'],
+})
+export class SidecontentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
+
